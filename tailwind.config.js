3d355fe38895b31cd1152e93cee2d386cea87e0e
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-06-05 18:59:25 -0400

    adding tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/*.liquid',
-    './templates/customers/*.liquid',
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: "90%",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "0",
+        lg: "0",
+        xl: "0",
+      },
+    },
+    colors: {
+      primary: "#323550",
+      "primary-alt": "#22253A",
+      "primary-light": "#4A4E69",
+      secondary: "#191B28",
+      tertiary: "#DAB279",
+      "tertiary-alt": "#967751",
+      quaternary: "#02040C",
+      light: "#FFFFFF",
+      medium: "#7C8A9A",
+    },
+    fontFamily: {
+      abril: ["Abril Fatface", "serif"],
+      lato: ["Lato", "serif"],
+      allura: ["Allura", "cursive"],
+    },
+    fontSize: {
+      xs: "0.6rem",
+      sm: "0.75rem",
+      base: "1rem",
+      lg: "1.5rem",
+      xl: "1.75rem",
+      "2xl": "2rem",
+      "3xl": "3rem",
+      "4xl": "4rem",
+      "5xl": "5rem",
+      "6xl": "6rem",
+    },
   },
-  plugins: [],
-}
-
+};
